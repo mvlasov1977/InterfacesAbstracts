4902673d80a73c978246fb56d6793f9ac4f91294
Maksim Vlasov <mvlasov1977@gmail.com>
2023-07-08 13:09:10 +0300

    exercises was finished

--- src/main/java/org/example/UsersDataManipulation.java
+++ src/main/java/org/example/UsersDataManipulation.java
@@ -10,15 +10,31 @@ public class UsersDataManipulation {
         return projectName;
     }
     public void sortByAgePutIntoNewCollection(ArrayList <User> inputCollection, ArrayList <User> outputCollection){
-        Comparator <User> comparatorAgeOfUser = (u1, u2) -> {
-            return u1.getAge() - u2.getAge();
-        };
-        inputCollection.stream().sorted(comparatorAgeOfUser).forEach(outputCollection::add);
+        try {
+            Comparator <User> comparatorAgeOfUser = (u1, u2) -> {
+                return u1.getAge() - u2.getAge();
+            };
+            inputCollection.stream().sorted(comparatorAgeOfUser).forEach(outputCollection::add);
+        }catch (Exception e){
+            // continue
+            //throw new ClassCastException("Input ArrayList is not User type!");
+        }
     }
     public Double getUsersAverageAge(ArrayList <User> inputCollection){
-        return inputCollection.stream().map(User::getAge).mapToDouble(i -> i).average().getAsDouble();
+        try {
+        if (!inputCollection.isEmpty()){
+            return inputCollection.stream().map(User::getAge).mapToDouble(i -> i).average().getAsDouble();
+        }else {
+            return 0.0;
+        }
+        }catch (Exception e){
+            return -1.0;
+            // continue
+            //throw new ClassCastException("Input ArrayList is not User type!");
+        }
     }
     public void sortByNameThenAge(ArrayList <User> inputCollection){
+        try {
         Comparator <User> comparatorNameAgeOfUser = (u1, u2) -> {
             if (u1.getFirstName().compareTo(u2.getFirstName()) == 0){
                 return u1.getAge() - u2.getAge();
@@ -26,16 +42,32 @@ public class UsersDataManipulation {
             return u1.getFirstName().compareTo(u2.getFirstName());
         };
         inputCollection.sort(comparatorNameAgeOfUser);
+        }catch (Exception e){
+            // continue
+            //throw new ClassCastException("Input ArrayList is not User type!");
+        }
     }
     public Boolean isSecondNameContainSorA(ArrayList <User> inputCollection){
+        try {
         return inputCollection.stream().map(i -> i)
                 .filter(i -> ( (i.getSecondName().charAt(0) == 'S') || (i.getSecondName().charAt(0) == 'A') ))
                 .toArray().length > 0;
+        }catch (Exception e){
+            return false;
+            // continue
+            //throw new ClassCastException("Input ArrayList is not User type!");
+        }
     }
     public Boolean isAllUsersHaveOver18Years(ArrayList <User> inputCollection){
+        try {
         return (inputCollection.stream()
                 .map(User::getAge)
                 .filter(i -> i<19)
                 .toArray().length) == 0;
+        }catch (Exception e){
+            return true;
+            // continue
+            //throw new ClassCastException("Input ArrayList is not User type!");
+        }
     }
 }
